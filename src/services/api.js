88f574031c38e624a3845ff21a499a3922f6f32f
Elaine-Tavares <elainetavares.developer@gmail.com>
2025-07-10 17:24:51 -0300

    atualização do api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,12 @@
 // src/services/api.js
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost/elaine_charm_backend"  // Caminho até minha pasta PHP no XAMPP
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost/elaine_charm_backend"  // Caminho até minha pasta PHP no XAMPP
+  baseURL: "https://elaines-charm.infinityfreeapp.com"
 });
 
 export default api;
