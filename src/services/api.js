400362f15b63486d1ced1f164ac4477455f39b89
Elaine-Tavares <elainetavares.developer@gmail.com>
2025-07-10 18:56:40 -0300

    testar o backend com proxy

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,16 @@ import axios from "axios";
 //   baseURL: "http://localhost/elaine_charm_backend"  // Caminho até minha pasta PHP no XAMPP
 // });
 
+// const api = axios.create({
+//   baseURL: "https://elaines-charm.infinityfreeapp.com"
+// });
+
+// Em src/services/api.js
 const api = axios.create({
-  baseURL: "https://elaines-charm.infinityfreeapp.com"
+  baseURL: "https://cors-anywhere.herokuapp.com/https://elaines-charm.infinityfreeapp.com",
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
